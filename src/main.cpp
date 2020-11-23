fb8bf06447ff1b47f9d83472760942d88f7a0ccc
Devang <sawantdevang9@gmail.com>
2020-11-23 07:49:25 -0500

    add a very basic project structure that prints hello world

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 23, 2020
+ *      Author: Devang Sawant
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world : this is devang" << std::endl;
+    return 0;
+}
+
+
